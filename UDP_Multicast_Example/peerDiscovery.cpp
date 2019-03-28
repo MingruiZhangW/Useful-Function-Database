b4d1c5dcb79cef2ba7bb9d32a681eb6a8eb0155c
MingruiZhangW <46324965+MingruiZhangW@users.noreply.github.com>
2019-03-28 16:53:44 -0400

    Update peerDiscovery.cpp

--- UDP_Multicast_Example/peerDiscovery.cpp
+++ UDP_Multicast_Example/peerDiscovery.cpp
@@ -18,7 +18,19 @@
 
 #include "peerDiscovery.h"
 
-void dht::PeerDiscovery::initialize_socket(int &__domain){
+namespace dht {
+
+PeerDiscovery::PeerDiscovery(int domain, in_port_t port, char *multicast_ip_group_address){
+
+    m_domain = domain;
+    m_port = port;
+    m_multicast_ip_group_address = multicast_ip_group_address;
+    m_continue_to_run = true;
+
+}
+
+void
+PeerDiscovery::initialize_socket(){
 
 #ifdef _WIN32
     // Initialize Windows Socket API with given VERSION.
@@ -28,8 +40,6 @@ void dht::PeerDiscovery::initialize_socket(int &__domain){
     }
 #endif
 
-    m_domain = __domain;
-    
     switch (m_domain)
     {
         case AF_INET:
@@ -60,7 +70,8 @@ void dht::PeerDiscovery::initialize_socket(int &__domain){
 
 }
 
-void dht::PeerDiscovery::initialize_socketopt(){
+void
+PeerDiscovery::initialize_socketopt(){
 
     m_opt = 1;
 
@@ -75,9 +86,8 @@ void dht::PeerDiscovery::initialize_socketopt(){
 
 }
 
-void dht::PeerDiscovery::initialize_sockaddr_Listener(int &port){
-
-    m_port = port;
+void
+PeerDiscovery::initialize_sockaddr_Listener(){
 
     switch (m_domain)
     {
@@ -101,10 +111,8 @@ void dht::PeerDiscovery::initialize_sockaddr_Listener(int &port){
 
 }
 
-void dht::PeerDiscovery::initialize_sockaddr_Sender(int &port, char* multicast_ip_group_address){
-
-    m_port = port;
-    m_multicast_ip_group_address = multicast_ip_group_address;
+void
+PeerDiscovery::initialize_sockaddr_Sender(){
 
     switch (m_domain)
     {
@@ -134,9 +142,8 @@ void dht::PeerDiscovery::initialize_sockaddr_Sender(int &port, char* multicast_i
 
 }
 
-void dht::PeerDiscovery::mcast_join(char* multicast_ip_group_address){
-
-    m_multicast_ip_group_address = multicast_ip_group_address;
+void
+PeerDiscovery::mcast_join(){
 
     switch (m_domain)
     {
@@ -189,7 +196,8 @@ void dht::PeerDiscovery::mcast_join(char* multicast_ip_group_address){
 
 }
 
-void dht::PeerDiscovery::m_sendto(uint8_t *buf,size_t &buf_size){
+void
+PeerDiscovery::m_sendto(uint8_t *buf,size_t &buf_size){
 
     switch (m_domain)
     {
@@ -234,7 +242,8 @@ void dht::PeerDiscovery::m_sendto(uint8_t *buf,size_t &buf_size){
 
 }
 
-void dht::PeerDiscovery::m_recvfrom(uint8_t *buf,size_t &buf_size){
+void
+PeerDiscovery::m_recvfrom(uint8_t *buf,size_t &buf_size){
 
     switch (m_domain)
     {
@@ -278,11 +287,12 @@ void dht::PeerDiscovery::m_recvfrom(uint8_t *buf,size_t &buf_size){
 
 }
 
-void dht::PeerDiscovery::Sender_start(int domain, int port, char *multicast_ip_group_address, dht::InfoHash nodeId, in_port_t port_to_send){
+void
+PeerDiscovery::Sender_oneTimeShoot(dht::InfoHash nodeId, in_port_t port_to_send){
 
     //Set up for Sender
-    this->initialize_socket(domain);
-    this->initialize_sockaddr_Sender(port,multicast_ip_group_address);
+    this->initialize_socket();
+    this->initialize_sockaddr_Sender();
 
     //Setup for send data
     uint8_t data_send[22];
@@ -291,7 +301,7 @@ void dht::PeerDiscovery::Sender_start(int domain, int port, char *multicast_ip_g
     size_t data_send_size = 22;
     int port_node = port_to_send;
     uint8_t port_node_binary[2];
-    dht::PeerDiscovery::inttolitend(port_node,port_node_binary);
+    PeerDiscovery::inttolitend(port_node,port_node_binary);
 
     //Copy Node id and node port
     memcpy (data_send, x, 20);
@@ -302,12 +312,13 @@ void dht::PeerDiscovery::Sender_start(int domain, int port, char *multicast_ip_g
 
 }
 
-void dht::PeerDiscovery::Listener_start(int domain, int port, char *multicast_ip_group_address){
+void
+PeerDiscovery::Listener_oneTimeShoot(){
 
-    this->initialize_socket(domain);
+    this->initialize_socket();
     this->initialize_socketopt();
-    this->initialize_sockaddr_Listener(port);
-    this->mcast_join(multicast_ip_group_address);
+    this->initialize_sockaddr_Listener();
+    this->mcast_join();
 
     size_t data_receive_size = 22;
     uint8_t data_receive[22];
@@ -320,15 +331,113 @@ void dht::PeerDiscovery::Listener_start(int domain, int port, char *multicast_ip
     data_port[0] = data_receive[20];
     data_port[1] = data_receive[21];
 
-    m_port_received = dht::PeerDiscovery::litendtoint(data_port);
+    m_port_received = PeerDiscovery::litendtoint(data_port);
     m_node_id_received = dht::InfoHash(data_infohash, 20);
 
 }
 
-dht::PeerDiscovery::~PeerDiscovery(){
+void
+PeerDiscovery::Sender_Loop(dht::InfoHash nodeId, in_port_t port_to_send){
+
+    //Set up for Sender
+    this->initialize_socket();
+    this->initialize_sockaddr_Sender();
+
+    //Setup for send data
+    uint8_t data_send[22];
+    uint8_t *x = nodeId.data();
+
+    size_t data_send_size = 22;
+    int port_node = port_to_send;
+    uint8_t port_node_binary[2];
+    PeerDiscovery::inttolitend(port_node,port_node_binary);
+
+    //Copy Node id and node port
+    memcpy (data_send, x, 20);
+    data_send[20] = port_node_binary[0];
+    data_send[21] = port_node_binary[1];
+
+    m_running = std::thread([&,this]{
+
+        while(m_continue_to_run){
+            
+            std::cout<<this->m_port<<std::endl;
+            this->m_sendto(data_send,data_send_size);
+            sleep(3);
+
+        }
+    
+    });
+
+    m_running.join();
+
+}
+
+void 
+PeerDiscovery::Listener_Loop(){
+
+    this->initialize_socket();
+    this->initialize_socketopt();
+    this->initialize_sockaddr_Listener();
+    this->mcast_join();
+
+    m_running = std::thread([&,this]{
+
+        while(m_continue_to_run){
+            
+            size_t data_receive_size = 22;
+            uint8_t data_receive[22];
+
+            this->m_recvfrom(data_receive,data_receive_size);
+
+            uint8_t data_infohash[20];
+            uint8_t data_port[2];
+
+            memcpy (data_infohash, data_receive, 20);
+            data_port[0] = data_receive[20];
+            data_port[1] = data_receive[21];
+
+            m_port_received = PeerDiscovery::litendtoint(data_port);
+            m_node_id_received = dht::InfoHash(data_infohash, 20);
+
+            NodeExport new_nodeexport;
+            new_nodeexport.id = m_node_id_received;
+            switch (m_domain)
+            {
+                case AF_INET:
+                    
+                    mempcpy(&new_nodeexport.ss,&m_addr_ipv4,sizeof(m_addr_ipv4));
+                    new_nodeexport.sslen = sizeof(m_addr_ipv4);
+                    break;
+            
+                case AF_INET6:
+
+                    mempcpy(&new_nodeexport.ss,&m_addr_ipv6,sizeof(m_addr_ipv6));
+                    new_nodeexport.sslen = sizeof(m_addr_ipv6);
+                    break;
+            }
+            m_node_vec.push_back(new_nodeexport);
+            
+            //bootstrap(m_node_vec);
+
+            m_node_vec.clear();
+            std::cout<<m_port_received<<std::endl;
+            sleep(1);
+
+        }
+    
+    });
+    m_running.join();
+
+}
+
+PeerDiscovery::~PeerDiscovery()
+{
 
 #ifdef _WIN32
     WSACleanup();
 #endif
+
+}
 
 }
